04d6be32ae24d4f4d68fb2e7208455bd41fb4c16
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-07-24 14:04:27 -0500

    leyendo datos de formulario

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 // instanciar ambas clases
 const eventBrite = new EventBrite();
 const ui = new Interfaz();
+
+
+// listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+
+    e.preventDefault();
+
+    // leer el texto del input buscar
+    const textoBuscador = document.getElementById('evento').value;
+
+    // leer select
+    const categorias = document.getElementById('listado-categorias');
+    const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+    
+    // validar buscador
+    if(textoBuscador !== ''){
+
+    }else{
+
+    }
+    
+});
