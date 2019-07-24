b41bd6d620aab41432348806db41846754c006b8
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-07-24 13:56:18 -0500

    leyendo categorias de eventos

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -2,7 +2,7 @@ class EventBrite{
 
     constructor(){
 
-        this.token_auth = 'GK76V7IXV5EMWHYSXHQC';
+        this.token_auth = 'LGAUMGKTBJXSL4Y7VTET';
         this.ordenar = 'date';
     }
 
@@ -10,6 +10,12 @@ class EventBrite{
     async obtenerCategorias(){
 
         // consultar categorias
+        const respuestaCategorias = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`);
 
+        // esperar respuesta de categorias
+        const categorias = await respuestaCategorias.json();
+
+        // devolver resultado
+        return categorias;
     }
 }
--- js/interfaz.js
+++ js/interfaz.js
@@ -13,10 +13,30 @@ class Interfaz{
     //Método para cuando inicie la app
     init(){
 
+        this.imprimirCategorias();
     }
 
     imprimirCategorias(){
 
-        const listaCategorias = eventBrite.obtenerCategorias();
+        const listaCategorias = eventBrite.obtenerCategorias()
+              .then( categorias => {
+                  
+                // seleccionar select categorias
+                const selectCategoria = document.getElementById('listado-categorias');
+
+                const categoria = categorias.categories;
+
+                // recorremos el arreglo e imprimimos categorias
+                categoria.forEach( categoria => {
+
+                    const option = document.createElement('option');
+                    option.value = categoria.id;
+
+                    option.appendChild(document.createTextNode(categoria.name_localized));
+
+                    selectCategoria.appendChild(option);
+                });
+              })
+
     }
 }
